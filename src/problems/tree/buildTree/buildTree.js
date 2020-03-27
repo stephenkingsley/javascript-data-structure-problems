c5b12a3d09b6dbdd42a20f1f120ee944b524eaad
stephenkingsley <stephenkingsley@foxmail.com>
2020-03-27 14:54:30 +0800

    feat: buildTreeByInorderAndPostorder

--- src/problems/tree/buildTree/buildTree.js
+++ src/problems/tree/buildTree/buildTree.js
@@ -9,12 +9,13 @@ function TreeNode(val) {
   this.val = val;
   this.left = this.right = null;
 }
+
 /**
  * @param {number[]} preorder
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function(preorder, inorder) {
+var buildTreeByPreorderAndInorder = function(preorder, inorder) {
   if (preorder.length <= 0 || inorder.length <= 0) {
     return null;
   }
@@ -37,6 +38,35 @@ var buildTree = function(preorder, inorder) {
 
   root.left = leftTree;
   root.right = rightTree;
+
+  return root;
+};
+
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTreeByInorderAndPostorder = function(inorder, postorder) {
+  if (inorder.length <= 0 || postorder.length <= 0) {
+    return null;
+  }
+
+  const root = new TreeNode(postorder[postorder.length - 1]);
+  const rootIndexInInOrder = inorder.findIndex(ele => ele === root.val);
+  
+  const leftTreeNodes = inorder.slice(0, rootIndexInInOrder);
+  const rightTreeNodes = inorder.slice(rootIndexInInOrder + 1, inorder.length);
+
+  root.left = buildTree(
+    leftTreeNodes,
+    postorder.filter(ele => leftTreeNodes.indexOf(ele) > -1),
+  );
+
+  root.right = buildTree(
+    rightTreeNodes,
+    postorder.filter(ele => rightTreeNodes.indexOf(ele) > -1),
+  );
 
   return root;
 };
